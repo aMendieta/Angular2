ff09afb2a8cfb445247336cdef2a871cf43a629f
AntonioDiaz-Regañon <antonio.diaz@sopra.com>
2016-11-17 15:20:53 +0100

    añado servidor node para desplegra en heroku

---
+++ app.js
@@ -0,0 +1,31 @@
+//server.js
+
+var express     = require('express');
+var app         = express();
+
+
+// Configuración
+app.configure(function() {
+    // Localización de los ficheros estÃ¡ticos
+    app.use(express.static(__dirname + '/public'));
+    // Muestra un log de todos los request en la consola
+    app.use(express.logger('dev'));
+    // Permite cambiar el HTML con el método POST
+    app.use(express.bodyParser());
+    // Simula DELETE y PUT
+    app.use(express.methodOverride());
+});
+
+// Carga una vista HTML simple donde irá nuestra Single App Page
+// Angular Manejará el Frontend
+app.get('*', function(req, res) {
+    res.sendfile('./index.html');
+});
+
+
+// Escucha en el puerto 8080 y corre el server
+//app.set('port', (process.env.PORT || 5000))
+//.listen(process.env.PORT || 5000)
+app.listen(process.env.PORT || 5000, function() {
+    console.log('App listening on port 9080');
+});
